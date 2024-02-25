98a5057a9d80f9c6ec8fdfa90b50173b77d29c1f
henriqueritter <henrique-ritter@hotmail.com>
2024-02-25 15:28:25 -0300

    ci: using env variables to database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,17 +2,19 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SongsModule } from './songs/songs.module';
 import { Song } from './songs/entities/song.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     SongsModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'root',
-      database: 'songdatahub',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
       entities: [Song],
       synchronize: true,
       logging: true,
